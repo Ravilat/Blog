eba92c26647100bd91e5f9e438010b565d55031a
Ravilat <faritych@yandex.ru>
2025-08-09 15:54:05 +0400

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Dockerfile

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO users(login, password, email)
+VALUES ('Rav', '$2a$10$UZEKavVwEu1tHcFbIMJZQu5Oa8kI4MrXikg3UYmv10l5WT8mra/xO', 'ravv@mail.ru');
+
+INSERT INTO posts (title, content, author_id, create_date, update_date)
+VALUES ('Изучение Spring', 'Как я учил Spring...', 1, '2025-07-30 10:00:00', '2025-07-30 10:00:00');
